e56ea76d1e3caaf20f28853e6f6d05aa0efeeb95
Freeman Chen <freemanchen115@gmail.com>
2024-07-13 18:13:25 -0700

    change

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import torch
 from dotenv import load_dotenv
 from src.model import ClimbNet
+from src.generator import ClimbGenerator
 from src.utils import OutputConversion
 
 load_dotenv()
@@ -20,6 +21,33 @@ def classification():
     return render_template('classification.html')
 @app.route('/generator')
 def generator():
+    input_grade = request.args.get('grade')
+    grade_to_level = {
+        'V4': 'easy', 'V5': 'easy',
+        'V6': 'medium', 'V7': 'medium',
+        'V8': 'hard', 'V9': 'hard',
+        'V10': 'very_hard', 'V10+':'very_hard', 'V11': 'very_hard', 'V12': 'very_hard', 'V13': 'very_hard'
+    }
+
+    level = grade_to_level[input_grade]
+
+    generator = ClimbGenerator(level)
+    clf = ClimbNet()
+    weights_filepath = "src/climbnet_weights.pth"
+    clf.load_state_dict(torch.load(weights_filepath))
+
+    clf.eval()
+    while True:
+        climb, labels, vector = generator.generate_climb()
+        vector = torch.tensor(vector).to(torch.float32).flatten()
+        with torch.no_grad():
+            soft_pred = clf(vector)
+        hard_pred = int(soft_pred.argmax())
+        out_conv = OutputConversion()
+        predicted_grade = out_conv.convert(hard_pred)
+
+        if predicted_grade == input_grade:
+            break
     return render_template('generator.html')
 
 @app.route('/api/save-positions', methods=['POST'])
