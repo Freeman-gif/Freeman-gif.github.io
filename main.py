18f2745a250087548ad13ee6d6b0a4d2c94045e0
Max Sivolella <maxsivolella@gmail.com>
2024-07-17 13:59:31 -0700

    changed home page

--- main.py
+++ main.py
@@ -25,35 +25,36 @@ def classification():
 
 @app.route('/generator')
 def generator():
-    input_grade = request.args.get('grade')
-    grade_to_level = {
-        'V4': 'easy', 'V5': 'easy',
-        'V6': 'medium', 'V7': 'medium',
-        'V8': 'hard', 'V9': 'hard',
-        'V10': 'very_hard', 'V10+': 'very_hard', 'V11':
-        'very_hard', 'V12': 'very_hard', 'V13': 'very_hard'
-    }
+    # input_grade = request.args.get('grade')
+    # grade_to_level = {
+    #     'V4': 'easy', 'V5': 'easy',
+    #     'V6': 'medium', 'V7': 'medium',
+    #     'V8': 'hard', 'V9': 'hard',
+    #     'V10': 'very_hard', 'V10+': 'very_hard', 'V11':
+    #     'very_hard', 'V12': 'very_hard', 'V13': 'very_hard'
+    # }
 
-    level = grade_to_level[input_grade]
+    # level = grade_to_level[input_grade]
 
-    generator = ClimbGenerator(level)
-    clf = ClimbNet()
-    weights_filepath = "src/climbnet_weights.pth"
-    clf.load_state_dict(torch.load(weights_filepath))
+    # generator = ClimbGenerator(level)
+    # clf = ClimbNet()
+    # weights_filepath = "src/climbnet_weights.pth"
+    # clf.load_state_dict(torch.load(weights_filepath))
 
-    clf.eval()
-    while True:
-        climb, labels, vector = generator.generate_climb()
-        vector = torch.tensor(vector).to(torch.float32).flatten()
-        with torch.no_grad():
-            soft_pred = clf(vector)
-        hard_pred = int(soft_pred.argmax())
-        out_conv = OutputConversion()
-        predicted_grade = out_conv.convert(hard_pred)
+    # clf.eval()
+    # while True:
+    #     climb, labels, vector = generator.generate_climb()
+    #     vector = torch.tensor(vector).to(torch.float32).flatten()
+    #     with torch.no_grad():
+    #         soft_pred = clf(vector)
+    #     hard_pred = int(soft_pred.argmax())
+    #     out_conv = OutputConversion()
+    #     predicted_grade = out_conv.convert(hard_pred)
 
-        if predicted_grade == input_grade:
-            break
+    #     if predicted_grade == input_grade:
+    #         break
     return render_template('generator.html')
+
 
 @app.route('/api/save-positions', methods=['POST'])
 def save_positions():
