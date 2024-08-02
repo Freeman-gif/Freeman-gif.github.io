36ee6ca2bb2f3b8012ad973ad61b54625ed8eb28
Freeman Chen <freemanchen115@gmail.com>
2024-08-02 15:27:37 -0700

    web

--- main.py
+++ main.py
@@ -23,37 +23,64 @@ def classification():
     return render_template('classification.html')
 
 
-@app.route('/generator')
+@app.route('/generator', methods=['GET', 'POST'])
 def generator():
-    # input_grade = request.args.get('grade')
-    # grade_to_level = {
-    #     'V4': 'easy', 'V5': 'easy',
-    #     'V6': 'medium', 'V7': 'medium',
-    #     'V8': 'hard', 'V9': 'hard',
-    #     'V10': 'very_hard', 'V10+': 'very_hard', 'V11':
-    #     'very_hard', 'V12': 'very_hard', 'V13': 'very_hard'
-    # }
+    options = ['V4', 'V5', 'V6', 'V7', 'V8', 'V9', 
+           'V10', 'V10+']
+    grade = None 
+    vector = None
+    input_grade = None
+    selected_cells = None
+    image = None
+    if request.method == 'POST':
+        if 'grade_dropdown' in request.form:
+            input_grade = request.form['grade_dropdown']
+    if input_grade:  # Only proceed if input_grade is not None
+        grade_to_level = {
+            'V4': 'easy', 'V5': 'easy',
+            'V6': 'medium', 'V7': 'medium',
+            'V8': 'hard', 'V9': 'hard',
+            'V10': 'very_hard', 'V10+': 'very_hard'
+        }
 
-    # level = grade_to_level[input_grade]
+        level = grade_to_level[input_grade]
 
-    # generator = ClimbGenerator(level)
-    # clf = ClimbNet()
-    # weights_filepath = "src/climbnet_weights.pth"
-    # clf.load_state_dict(torch.load(weights_filepath))
+        generator = ClimbGenerator(level)
+        clf = ClimbNet()
+        weights_filepath = "src/climbnet_weights.pth"
+        clf.load_state_dict(torch.load(weights_filepath))
 
-    # clf.eval()
-    # while True:
-    #     climb, labels, vector = generator.generate_climb()
-    #     vector = torch.tensor(vector).to(torch.float32).flatten()
-    #     with torch.no_grad():
-    #         soft_pred = clf(vector)
-    #     hard_pred = int(soft_pred.argmax())
-    #     out_conv = OutputConversion()
-    #     predicted_grade = out_conv.convert(hard_pred)
+        clf.eval()
+        while True:
+            climb, labels, vector = generator.generate_climb()
+            vector = torch.tensor(vector).to(torch.float32).flatten()
+            with torch.no_grad():
+                soft_pred = clf(vector)
+            hard_pred = int(soft_pred.argmax())
+            out_conv = OutputConversion()
+            predicted_grade = out_conv.convert(hard_pred)
 
-    #     if predicted_grade == input_grade:
-    #         break
-    return render_template('generator.html')
+            if predicted_grade == input_grade:
+                print(f"predicted_grade:{predicted_grade}")
+                break
+            
+            
+            x_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+            y_labels = [str(i) for i in range(1, 19)]
+
+            # List the selected cells
+            selected_cells = []
+            for idx, value in enumerate(vector):
+                if value == 1.0:
+                    row = idx // 11
+                    col = idx % 11
+                    label = f'{x_labels[col]}{y_labels[row]}'
+                    selected_cells.append(label)
+        
+            
+            
+    return render_template('generator+.html', grade=input_grade, vec=selected_cells,img_path=image)#)input_grade=input_grade)
+
 
 
 @app.route('/api/save-positions', methods=['POST'])
